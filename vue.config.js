4371b66bf2adda622c3e6a5929c9681d8ab202e1
songshuzhong <sshuzhong@outlook,com>
2021-10-29 18:34:53 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,32 @@
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
-  publicPath: isDev ? '' : '/umis-website/dist',
+  publicPath: isDev ? "" : "/umis-website/dist",
+  configureWebpack: {
+    output: {
+      filename:
+        process.env.NODE_ENV === "production"
+          ? "[name].[chunkhash].js"
+          : "[name].js",
+      chunkFilename: "[name].[chunkhash].js"
+    },
+    resolve: {
+      alias: {
+        "@umis-renderer": "../../../umis-renderer"
+      }
+    }
+  },
   devServer: {
     disableHostCheck: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:1026',
-        changeOrigin: true,
+      "/api": {
+        target: "http://localhost:1026",
+        changeOrigin: true
       },
-      '/ajax': {
-        target: 'http://kadm.test.weibo.com',
-        changeOrigin: true,
-      },
-    },
-  },
+      "/ajax": {
+        target: "http://kadm.test.weibo.com",
+        changeOrigin: true
+      }
+    }
+  }
 };
