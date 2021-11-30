a914e8ad177f905781bc8b76ba227f8d3cf92b68
songshuzhong <sshuzhong@outlook,com>
2021-11-30 18:29:08 +0800

    dll

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,4 @@
-const path = require('path');
-const webpack = require('webpack');
-const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'dev';
-
-const dllReference = (config) => {
-  config.plugin('vendorDll')
-    .use(webpack.DllReferencePlugin, [{
-      context: __dirname,
-      manifest: require('./public/dll/vendor.manifest.json')
-    }]);
-  config.plugin('addAssetHtml')
-    .use(AddAssetHtmlPlugin, [
-      [
-        {
-          filepath: require.resolve(path.resolve(__dirname, 'public/dll/vendor.dll.js')),
-          outputPath: 'dist/dll',
-          publicPath: '/dist/dll'
-        }
-      ]
-    ])
-    .after('html');
-};
 
 module.exports = {
   publicPath: isDev ? './' : '/umis-website/dist',
@@ -39,11 +17,6 @@ module.exports = {
     },
     optimization: {
       minimize: !isDev
-    }
-  },
-  chainWebpack(config) {
-    if(process.env.NODE_ENV === 'production'){
-      dllReference(config);
     }
   },
   css: {
