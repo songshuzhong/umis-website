b62fe5704b73e19a32c076dd18828b55be16f9f9
songshuzhong <sshuzhong@outlook,com>
2021-12-31 19:47:34 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <el-config-provider :locale="locale">
-    <router-view />
+    <router-view/>
   </el-config-provider>
 </template>
 
 <script>
-import { ElConfigProvider } from 'element-plus';
+import {onMounted} from 'vue';
+import {ElConfigProvider, ElMessageBox} from 'element-plus';
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 
 export default {
@@ -14,6 +15,30 @@ export default {
     [ElConfigProvider.name]: ElConfigProvider
   },
   setup() {
+    onMounted(() => {
+      try {
+        if ('serviceWorker' in navigator) {
+          navigator.serviceWorker.getRegistration().then(reg => {
+            // eslint-disable-next-line no-debugger
+            debugger;
+            ElMessageBox.confirm(
+              '确定要升级吗?',
+              'Warning',
+              {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning',
+              }
+            ).then(() => {
+              reg.waiting.postMessage('skipWaiting');
+            });
+          });
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    });
+
     return {
       locale: zhCn
     };
--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,32 +1,36 @@
-/* eslint-disable no-console */
-
-import { register } from 'register-service-worker';
-
-if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      );
-    },
-    registered () {
-      console.log('Service worker has been registered.');
-    },
-    cached () {
-      console.log('Content has been cached for offline use.');
-    },
-    updatefound () {
-      console.log('New content is downloading.');
-    },
-    updated () {
-      console.log('New content is available; please refresh.');
-    },
-    offline () {
-      console.log('No internet connection found. App is running in offline mode.');
-    },
-    error (error) {
-      console.error('Error during service worker registration:', error);
-    }
-  });
+function emitUpdate() {
+  const event = document.createEvent('Event');
+  event.initEvent('sw.update', true, true);
+  window.dispatchEvent(event);
 }
+
+window.addEventListener('load', function () {
+  if ('serviceWorker' in navigator) {
+    navigator
+      .serviceWorker
+      .register(`${process.env.BASE_URL}/service-worker.js`)
+      .then(function (reg) {
+        // eslint-disable-next-line no-debugger
+        debugger;
+        if (reg.waiting) {
+          emitUpdate();
+          return;
+        }
+        reg.onupdatefound = function () {
+          const installingWorker = reg.installing;
+          installingWorker.onstatechange = function () {
+            switch (installingWorker.state) {
+            case 'installed':
+              if (navigator.serviceWorker.controller) {
+                emitUpdate();
+              }
+              break;
+            }
+          };
+        };
+      })
+      .catch(function (e) {
+        console.error('Error during service worker registration:', e);
+      });
+  }
+});
