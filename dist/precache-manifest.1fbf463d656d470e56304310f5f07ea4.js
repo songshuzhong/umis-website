505893efcfc81c0b452331de74069aa933cc99af
songshuzhong <sshuzhong@outlook,com>
2022-03-23 09:47:16 +0800

    cdn

---
+++ dist/precache-manifest.1fbf463d656d470e56304310f5f07ea4.js
@@ -0,0 +1,314 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e426204abe7a49e93a68",
+    "url": "/umis-website/dist/chunk/chunk-2d225f49.64d221.js"
+  },
+  {
+    "revision": "df69d5793cabde3e7ba6",
+    "url": "/umis-website/dist/chunk/chunk-vendors.5182fa.js"
+  },
+  {
+    "revision": "4d11105250c51ede6bd6",
+    "url": "/umis-website/dist/chunk/copy-to-clipboard.ea37e8.js"
+  },
+  {
+    "revision": "17cf8d3cf65fa3c46b09",
+    "url": "/umis-website/dist/chunk/less.d572eb.js"
+  },
+  {
+    "revision": "006314fdaa2c709f44e2",
+    "url": "/umis-website/dist/chunk/monaco-editor.c64f32.js"
+  },
+  {
+    "revision": "bcfcd0f99508ac718ff2",
+    "url": "/umis-website/dist/css/app.6cf78a85.css"
+  },
+  {
+    "revision": "df69d5793cabde3e7ba6",
+    "url": "/umis-website/dist/css/chunk-vendors.e9d08994.css"
+  },
+  {
+    "revision": "006314fdaa2c709f44e2",
+    "url": "/umis-website/dist/css/monaco-editor.719b2d48.css"
+  },
+  {
+    "revision": "397b340813d08a9489f90a8ee2f1e652",
+    "url": "/umis-website/dist/dll/vendor.dll.js"
+  },
+  {
+    "revision": "f7b8786465949f24c28e9635c4991fab",
+    "url": "/umis-website/dist/dll/vendor.manifest.json"
+  },
+  {
+    "revision": "805fb6ad1751ed8b849b5bf9be742ab5",
+    "url": "/umis-website/dist/fonts/codicon.805fb6ad.ttf"
+  },
+  {
+    "revision": "ce9bbdf78b2c215adf3c80bc4534ce4c",
+    "url": "/umis-website/dist/img/loading.ce9bbdf7.svg"
+  },
+  {
+    "revision": "d60247266d21a5e4317fd9cf952472e8",
+    "url": "/umis-website/dist/index.html"
+  },
+  {
+    "revision": "bcfcd0f99508ac718ff2",
+    "url": "/umis-website/dist/js/app.33e268.js"
+  },
+  {
+    "revision": "ce9bbdf78b2c215adf3c80bc4534ce4c",
+    "url": "/umis-website/dist/loading.svg"
+  },
+  {
+    "revision": "69f40570a636b8bb8d8cff3ec21b2661",
+    "url": "/umis-website/dist/manifest.json"
+  },
+  {
+    "revision": "8ce054679bd6eb5388347964318dde50",
+    "url": "/umis-website/dist/thumbnail/Action.svg"
+  },
+  {
+    "revision": "a105412541a27b925d31de8bba66bfe5",
+    "url": "/umis-website/dist/thumbnail/Alert.svg"
+  },
+  {
+    "revision": "00d001c7f1f42b49c784a1b54f38b988",
+    "url": "/umis-website/dist/thumbnail/Aside.svg"
+  },
+  {
+    "revision": "dffc7b31ce3774027726e59e85ed0c3a",
+    "url": "/umis-website/dist/thumbnail/Avatar.svg"
+  },
+  {
+    "revision": "1c598a8001ae1361be0be015efa32547",
+    "url": "/umis-website/dist/thumbnail/Breadcrumb.svg"
+  },
+  {
+    "revision": "a5619a2e66aeb193988e82dd38e687b4",
+    "url": "/umis-website/dist/thumbnail/Calendar.svg"
+  },
+  {
+    "revision": "81044ac43d50aa006e1e763e24cc1b42",
+    "url": "/umis-website/dist/thumbnail/Card.svg"
+  },
+  {
+    "revision": "6c636176403ed76b65216578cfede70d",
+    "url": "/umis-website/dist/thumbnail/Cards.svg"
+  },
+  {
+    "revision": "8028e11855515244750bb1fe1455ae44",
+    "url": "/umis-website/dist/thumbnail/Carousel.svg"
+  },
+  {
+    "revision": "e8f18dc96cfe559fe5f5741b39ae5d0a",
+    "url": "/umis-website/dist/thumbnail/Cascader.svg"
+  },
+  {
+    "revision": "6710d20b1cb7e39423db7791db05caf4",
+    "url": "/umis-website/dist/thumbnail/Chart.svg"
+  },
+  {
+    "revision": "6619c6071f23ad7cc886d8f944a85ec5",
+    "url": "/umis-website/dist/thumbnail/Checkbox.svg"
+  },
+  {
+    "revision": "29f9b52fccb77b96256ee7d5477d7b4f",
+    "url": "/umis-website/dist/thumbnail/Collapse.svg"
+  },
+  {
+    "revision": "3b5545573e4b3b8fe85a1b87c014b00c",
+    "url": "/umis-website/dist/thumbnail/Colorpicker.svg"
+  },
+  {
+    "revision": "3ec4e93bdd4009d0bfebd2aaabaee8f0",
+    "url": "/umis-website/dist/thumbnail/Combo.svg"
+  },
+  {
+    "revision": "0ae4e59b35225daefa6da756c55a4b58",
+    "url": "/umis-website/dist/thumbnail/Count.svg"
+  },
+  {
+    "revision": "7e8bf2ea36c1746e4c5329caf8ccb1ea",
+    "url": "/umis-website/dist/thumbnail/Data.svg"
+  },
+  {
+    "revision": "09909e210574af6ef6dc494d5c911230",
+    "url": "/umis-website/dist/thumbnail/Datepicker.svg"
+  },
+  {
+    "revision": "3887b7708419cdb1597d80825c30ece9",
+    "url": "/umis-website/dist/thumbnail/Dialog.svg"
+  },
+  {
+    "revision": "3a51eec1fcf31f2f5ed8a55daec29dc6",
+    "url": "/umis-website/dist/thumbnail/Divider.svg"
+  },
+  {
+    "revision": "568c37a660041c49f65e5f89c22623df",
+    "url": "/umis-website/dist/thumbnail/Drawer.svg"
+  },
+  {
+    "revision": "266eed66102fc798cd06f887bc224dda",
+    "url": "/umis-website/dist/thumbnail/Dropdown.svg"
+  },
+  {
+    "revision": "3972c5bd51498f392174455d22b96569",
+    "url": "/umis-website/dist/thumbnail/Each.svg"
+  },
+  {
+    "revision": "dd877d1e8ce1f1d5c00917bc3320fdb9",
+    "url": "/umis-website/dist/thumbnail/Empty.svg"
+  },
+  {
+    "revision": "00d001c7f1f42b49c784a1b54f38b988",
+    "url": "/umis-website/dist/thumbnail/Footer.svg"
+  },
+  {
+    "revision": "3ec4e93bdd4009d0bfebd2aaabaee8f0",
+    "url": "/umis-website/dist/thumbnail/Form.svg"
+  },
+  {
+    "revision": "9aafb4a06c6ab283113661c9655d79d5",
+    "url": "/umis-website/dist/thumbnail/Grid.svg"
+  },
+  {
+    "revision": "00d001c7f1f42b49c784a1b54f38b988",
+    "url": "/umis-website/dist/thumbnail/Header.svg"
+  },
+  {
+    "revision": "d986043645a2f0a3c92d5bae678b79e5",
+    "url": "/umis-website/dist/thumbnail/Html.svg"
+  },
+  {
+    "revision": "e0199bfb3140a5c9040160011d834ac7",
+    "url": "/umis-website/dist/thumbnail/Icon.svg"
+  },
+  {
+    "revision": "c6299b5d87d362eea701ca3a2c737903",
+    "url": "/umis-website/dist/thumbnail/Image.svg"
+  },
+  {
+    "revision": "c536cf3d41e15d35abc38315ba001b79",
+    "url": "/umis-website/dist/thumbnail/Input.svg"
+  },
+  {
+    "revision": "62508fd41dcc4fc0842ee09c7a8748cc",
+    "url": "/umis-website/dist/thumbnail/Layout.svg"
+  },
+  {
+    "revision": "00d001c7f1f42b49c784a1b54f38b988",
+    "url": "/umis-website/dist/thumbnail/List.svg"
+  },
+  {
+    "revision": "00d001c7f1f42b49c784a1b54f38b988",
+    "url": "/umis-website/dist/thumbnail/Main.svg"
+  },
+  {
+    "revision": "4315dcfec9e510ee8c25a40fabc793c5",
+    "url": "/umis-website/dist/thumbnail/Mapping.svg"
+  },
+  {
+    "revision": "35ab483cdf47b4a702176969b2e8f842",
+    "url": "/umis-website/dist/thumbnail/Menu.svg"
+  },
+  {
+    "revision": "a484361729922db25cbbf0061319b6f1",
+    "url": "/umis-website/dist/thumbnail/Number.svg"
+  },
+  {
+    "revision": "f38ed05f0418be7c842872de59779705",
+    "url": "/umis-website/dist/thumbnail/Page-header.svg"
+  },
+  {
+    "revision": "568c37a660041c49f65e5f89c22623df",
+    "url": "/umis-website/dist/thumbnail/Page.svg"
+  },
+  {
+    "revision": "feb0d896e0aa4925af3ecca5c84b51ea",
+    "url": "/umis-website/dist/thumbnail/Progress.svg"
+  },
+  {
+    "revision": "95bcb59854c18d449f53c407ce9a2e5f",
+    "url": "/umis-website/dist/thumbnail/Qrcode.svg"
+  },
+  {
+    "revision": "8b6ad7ab72c859d1c65082465f27bf29",
+    "url": "/umis-website/dist/thumbnail/Quickedit.svg"
+  },
+  {
+    "revision": "281a77f943163c81a546eae8c85e5dae",
+    "url": "/umis-website/dist/thumbnail/Radio.svg"
+  },
+  {
+    "revision": "c8d57e3afcd40cf5e9103e934214e1c6",
+    "url": "/umis-website/dist/thumbnail/Rate.svg"
+  },
+  {
+    "revision": "383faa40327564974635768ea1e6fda3",
+    "url": "/umis-website/dist/thumbnail/Result.svg"
+  },
+  {
+    "revision": "b249bb3340defca67e595bd9134fd12c",
+    "url": "/umis-website/dist/thumbnail/Select.svg"
+  },
+  {
+    "revision": "d986043645a2f0a3c92d5bae678b79e5",
+    "url": "/umis-website/dist/thumbnail/Service.svg"
+  },
+  {
+    "revision": "8caab9ac0670fde0c2e9f8024b22db9a",
+    "url": "/umis-website/dist/thumbnail/Silder.svg"
+  },
+  {
+    "revision": "ce78141f8ec9d9f0563b4673b62542ce",
+    "url": "/umis-website/dist/thumbnail/Static.svg"
+  },
+  {
+    "revision": "4a0b023c098ecc643489594097293555",
+    "url": "/umis-website/dist/thumbnail/Steps.svg"
+  },
+  {
+    "revision": "750f6b0f464bdbb7a6da1a6cc0b6b6b1",
+    "url": "/umis-website/dist/thumbnail/Switch.svg"
+  },
+  {
+    "revision": "cf5f3fbd51bb63c85ed47f14133a15ac",
+    "url": "/umis-website/dist/thumbnail/Table.svg"
+  },
+  {
+    "revision": "dae1b1d369936a55a573f5c150dd384d",
+    "url": "/umis-website/dist/thumbnail/Tabs.svg"
+  },
+  {
+    "revision": "d4cb4c86c39e62063ffc6406b2b6720f",
+    "url": "/umis-website/dist/thumbnail/Timeline.svg"
+  },
+  {
+    "revision": "f0b4e23cc23e9d9c5a1567d70b763933",
+    "url": "/umis-website/dist/thumbnail/Timepicker.svg"
+  },
+  {
+    "revision": "5f97dcb1901e081df736702fddb48f13",
+    "url": "/umis-website/dist/thumbnail/Todo.svg"
+  },
+  {
+    "revision": "953daea3c43db6f43a8a1c6ef2d84c65",
+    "url": "/umis-website/dist/thumbnail/Upload.svg"
+  },
+  {
+    "revision": "57850aaee20f102f921c6f82a6f86765",
+    "url": "/umis-website/dist/thumbnail/Video.svg"
+  },
+  {
+    "revision": "62508fd41dcc4fc0842ee09c7a8748cc",
+    "url": "/umis-website/dist/thumbnail/Wrapper.svg"
+  },
+  {
+    "revision": "42df35ae2586cf52694de9c9f9626917",
+    "url": "/umis-website/dist/worker/editor.worker.js"
+  },
+  {
+    "revision": "846bf2397893962ca0f98db83579a948",
+    "url": "/umis-website/dist/worker/json.worker.js"
+  }
+]);
