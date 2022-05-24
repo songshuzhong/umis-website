05c6e4ef465cf2cd86a42cacdfc91d2b5d24c0db
songshuzhong <sshuzhong@outlook,com>
2022-05-25 07:25:37 +0800

    remove font-awesome

--- dist/precache-manifest.77853f94afcceca2b860237e33660bad.js
+++ dist/precache-manifest.77853f94afcceca2b860237e33660bad.js
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/i-website/dist/chunk/chunk-36ebdaed.2eecc3.js"
   },
   {
-    "revision": "3e892e40b8022892271d",
-    "url": "/i-website/dist/chunk/chunk-vendors.653d66.js"
+    "revision": "b1e2aa686674b73c689c",
+    "url": "/i-website/dist/chunk/chunk-vendors.05c02d.js"
   },
   {
     "revision": "1dd85c9cd54bccaadcd7",
@@ -24,11 +24,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/i-website/dist/circle.svg"
   },
   {
-    "revision": "2920d42e4a6f4f4a2eea",
+    "revision": "8d3949f7ddad9b93f8af",
     "url": "/i-website/dist/css/app.8024d1f7.css"
   },
   {
-    "revision": "3e892e40b8022892271d",
+    "revision": "b1e2aa686674b73c689c",
     "url": "/i-website/dist/css/chunk-vendors.12d09677.css"
   },
   {
@@ -48,12 +48,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/i-website/dist/img/loading.ce9bbdf7.svg"
   },
   {
-    "revision": "2461459f5a652274911d9f5eb29bba55",
+    "revision": "7be930ecb6d8d00630bccb517b8ce659",
     "url": "/i-website/dist/index.html"
   },
   {
-    "revision": "2920d42e4a6f4f4a2eea",
-    "url": "/i-website/dist/js/app.05dcd5.js"
+    "revision": "8d3949f7ddad9b93f8af",
+    "url": "/i-website/dist/js/app.48067b.js"
   },
   {
     "revision": "ce9bbdf78b2c215adf3c80bc4534ce4c",
