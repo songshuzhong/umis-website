c96750250aa6968ed977871dc4bcc0332dc93d64
songshuzhong <sshuzhong@outlook,com>
2022-09-02 11:51:46 +0800

    update

--- dist/precache-manifest.a0470cdc29b8ba0f55fd5dd5d3e36826.js
+++ dist/precache-manifest.a0470cdc29b8ba0f55fd5dd5d3e36826.js
@@ -48,7 +48,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/i-website/dist/img/loading.dd9336d7.svg"
   },
   {
-    "revision": "6d43fae6943943925c62e5240be395ec",
+    "revision": "729d145372a6465612b8b5f8e90cf0c1",
     "url": "/i-website/dist/index.html"
   },
   {
