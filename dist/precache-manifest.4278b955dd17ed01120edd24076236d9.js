26030d8469f5b82fb2fe5126660de2532fa1a0bf
songshuzhong <sshuzhong@outlook,com>
2022-10-31 19:15:11 +0800

    update

--- dist/precache-manifest.4278b955dd17ed01120edd24076236d9.js
+++ dist/precache-manifest.4278b955dd17ed01120edd24076236d9.js
@@ -24,7 +24,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/i-website/dist/circle.svg"
   },
   {
-    "revision": "022f97e1c9267e92c7f1",
+    "revision": "18b392b447d8f9790b98",
     "url": "/i-website/dist/css/app.e4068e06.css"
   },
   {
@@ -48,12 +48,12 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/i-website/dist/img/loading.dd9336d7.svg"
   },
   {
-    "revision": "f1f2496832ccf19bbd248cc47b462b4d",
+    "revision": "cf680d858872526694aea515fb576ac0",
     "url": "/i-website/dist/index.html"
   },
   {
-    "revision": "022f97e1c9267e92c7f1",
-    "url": "/i-website/dist/js/app.682feb.js"
+    "revision": "18b392b447d8f9790b98",
+    "url": "/i-website/dist/js/app.a6c279.js"
   },
   {
     "revision": "3866f3ee76597fcdadc88a83a339fdae",
