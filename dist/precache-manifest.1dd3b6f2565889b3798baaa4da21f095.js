9956ae39f2cc32eef0f04230799fc4fe8c926d40
songshuzhong <sshuzhong@outlook,com>
2023-05-09 12:50:32 +0800

    u

---
+++ dist/precache-manifest.1dd3b6f2565889b3798baaa4da21f095.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503e9282c7e11a4677a0",
+    "url": "/i-website/dist/chunk/chunk-2d0bdf0d.b07b56.js"
+  },
+  {
+    "revision": "c7b14fba59724b48bfbf",
+    "url": "/i-website/dist/chunk/chunk-2d0c9159.c9bf4c.js"
+  },
+  {
+    "revision": "2594ee8a6bcffafd9a92",
+    "url": "/i-website/dist/chunk/chunk-2d20fe5f.cbd064.js"
+  },
+  {
+    "revision": "fa5dfea3f2af8ea19f4a",
+    "url": "/i-website/dist/chunk/chunk-2d225f49.1a8097.js"
+  },
+  {
+    "revision": "6f1b1b7d7aa21cea85fa",
+    "url": "/i-website/dist/chunk/chunk-44b38984.1668f2.js"
+  },
+  {
+    "revision": "5a05c88014ce26eb157a",
+    "url": "/i-website/dist/chunk/chunk-4b5f9539.eb7606.js"
+  },
+  {
+    "revision": "3cd52f486c275e2e6ec8",
+    "url": "/i-website/dist/chunk/chunk-7fed599b.a04c2d.js"
+  },
+  {
+    "revision": "71a0be0b851a93081022",
+    "url": "/i-website/dist/chunk/chunk-b2126b70.6a540e.js"
+  },
+  {
+    "revision": "8909b8f4ddb26c4c2d11",
+    "url": "/i-website/dist/chunk/chunk-fcdd8a66.35b536.js"
+  },
+  {
+    "revision": "34cd4a3cf88f62ac425d",
+    "url": "/i-website/dist/chunk/chunk-vendors.783bfb.js"
+  },
+  {
+    "revision": "c9523e4cf8eeb1e220c9",
+    "url": "/i-website/dist/chunk/editor.2187b2.js"
+  },
+  {
+    "revision": "d051a14d9b030a5d5f44",
+    "url": "/i-website/dist/chunk/fontawesome-brands.8b1edc.js"
+  },
+  {
+    "revision": "83593eb8e206232af03a",
+    "url": "/i-website/dist/chunk/fontawesome-regular.c675fd.js"
+  },
+  {
+    "revision": "4cd937aa3aab9489aab7",
+    "url": "/i-website/dist/chunk/fontawesome-solid.a341c9.js"
+  },
+  {
+    "revision": "e39fc80d8e9ac758a1c7",
+    "url": "/i-website/dist/chunk/fontawesome.172310.js"
+  },
+  {
+    "revision": "37b4a0f48e0b1de65883",
+    "url": "/i-website/dist/chunk/vue-fontawesome.b89287.js"
+  },
+  {
+    "revision": "ae48e2078551e8a64156f767f51e5254",
+    "url": "/i-website/dist/circle.svg"
+  },
+  {
+    "revision": "6f1b1b7d7aa21cea85fa",
+    "url": "/i-website/dist/css/chunk-44b38984.0d357c10.css"
+  },
+  {
+    "revision": "5a05c88014ce26eb157a",
+    "url": "/i-website/dist/css/chunk-4b5f9539.0d357c10.css"
+  },
+  {
+    "revision": "3cd52f486c275e2e6ec8",
+    "url": "/i-website/dist/css/chunk-7fed599b.a9b80e5f.css"
+  },
+  {
+    "revision": "34cd4a3cf88f62ac425d",
+    "url": "/i-website/dist/css/chunk-vendors.c3d1b1dc.css"
+  },
+  {
+    "revision": "04dbdb333f21188b1c39",
+    "url": "/i-website/dist/css/home.7ba3e2d3.css"
+  },
+  {
+    "revision": "fe372c245b6395f157df",
+    "url": "/i-website/dist/css/index.cfb1ea96.css"
+  },
+  {
+    "revision": "e85a80130a61754bfd7b",
+    "url": "/i-website/dist/css/playground.a2833d96.css"
+  },
+  {
+    "revision": "805fb6ad1751ed8b849b5bf9be742ab5",
+    "url": "/i-website/dist/fonts/codicon.805fb6ad.ttf"
+  },
+  {
+    "revision": "960fac4bb20254b0c3d333e654ada9b3",
+    "url": "/i-website/dist/home.html"
+  },
+  {
+    "revision": "ae48e2078551e8a64156f767f51e5254",
+    "url": "/i-website/dist/img/circle.ae48e207.svg"
+  },
+  {
+    "revision": "25b99dcac91b4c512353eccd69f391e4",
+    "url": "/i-website/dist/img/forbidden.25b99dca.jpeg"
+  },
+  {
+    "revision": "06512621b564df8be841598d77e6e195",
+    "url": "/i-website/dist/img/not-found.06512621.jpeg"
+  },
+  {
+    "revision": "dbc349018f7cb701744c72498ddac3c6",
+    "url": "/i-website/dist/index.html"
+  },
+  {
+    "revision": "04dbdb333f21188b1c39",
+    "url": "/i-website/dist/js/home.571ba0.js"
+  },
+  {
+    "revision": "fe372c245b6395f157df",
+    "url": "/i-website/dist/js/index.14993f.js"
+  },
+  {
+    "revision": "e85a80130a61754bfd7b",
+    "url": "/i-website/dist/js/playground.e99d5a.js"
+  },
+  {
+    "revision": "3866f3ee76597fcdadc88a83a339fdae",
+    "url": "/i-website/dist/manifest.json"
+  },
+  {
+    "revision": "e80dd7439c5661b46b3525e9171d664e",
+    "url": "/i-website/dist/playground.html"
+  },
+  {
+    "revision": "9df39e09fcb3cb9b413a46f9ab221d71",
+    "url": "/i-website/dist/shuttle.png"
+  },
+  {
+    "revision": "96ae88e617004c7b4b4b8f42ac475a74",
+    "url": "/i-website/dist/worker/editor.worker.js"
+  },
+  {
+    "revision": "55a39908513ae7c0fc39a5f1ca0a8088",
+    "url": "/i-website/dist/worker/json.worker.js"
+  }
+]);
