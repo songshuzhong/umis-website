a540366923e1d36fd71a39eb0a933d3b19be8b53
songshuzhong <sshuzhong@outlook,com>
2023-06-23 06:43:20 +0800

    update

--- dist/precache-manifest.04661456d924c7c1a180ff070801b091.js
+++ dist/precache-manifest.04661456d924c7c1a180ff070801b091.js
@@ -16,55 +16,55 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/i-website/dist/chunk/chunk-2d225f49.1a8097.js"
   },
   {
-    "revision": "1e8b45a19d8f5d563816",
-    "url": "/i-website/dist/chunk/chunk-3bdd9743.657772.js"
-  },
-  {
-    "revision": "e2af8e05e34f86df17de",
-    "url": "/i-website/dist/chunk/chunk-961551d6.feb587.js"
+    "revision": "8bea1d651e8816e95925",
+    "url": "/i-website/dist/chunk/chunk-38044ab2.ab6d3f.js"
   },
   {
     "revision": "71a0be0b851a93081022",
     "url": "/i-website/dist/chunk/chunk-b2126b70.6a540e.js"
   },
   {
+    "revision": "dc87c164141ddf0160da",
+    "url": "/i-website/dist/chunk/chunk-c06ba2b4.bec0df.js"
+  },
+  {
     "revision": "8909b8f4ddb26c4c2d11",
     "url": "/i-website/dist/chunk/chunk-fcdd8a66.35b536.js"
   },
   {
-    "revision": "5c0dcdc2a1d1bd9c873f",
-    "url": "/i-website/dist/chunk/chunk-vendors.2d4c61.js"
+    "revision": "a557bec8da30fbf36ac1",
+    "url": "/i-website/dist/chunk/chunk-vendors.84565a.js"
   },
   {
-    "revision": "8c32fbd3c37380c56b5d",
-    "url": "/i-website/dist/chunk/editor.a6843b.js"
+    "revision": "2eddba12d6d1bd085b31",
+    "url": "/i-website/dist/chunk/editor.b1231a.js"
   },
   {
     "revision": "ae48e2078551e8a64156f767f51e5254",
     "url": "/i-website/dist/circle.svg"
   },
   {
-    "revision": "1e8b45a19d8f5d563816",
-    "url": "/i-website/dist/css/chunk-3bdd9743.90717131.css"
+    "revision": "8bea1d651e8816e95925",
+    "url": "/i-website/dist/css/chunk-38044ab2.90717131.css"
   },
   {
-    "revision": "e2af8e05e34f86df17de",
-    "url": "/i-website/dist/css/chunk-961551d6.ba4ddaba.css"
+    "revision": "dc87c164141ddf0160da",
+    "url": "/i-website/dist/css/chunk-c06ba2b4.ba4ddaba.css"
   },
   {
-    "revision": "5c0dcdc2a1d1bd9c873f",
-    "url": "/i-website/dist/css/chunk-vendors.b2a3a2c7.css"
+    "revision": "a557bec8da30fbf36ac1",
+    "url": "/i-website/dist/css/chunk-vendors.4bb81b51.css"
   },
   {
-    "revision": "658b69573aa84ab1932c",
+    "revision": "46c347e31279929209db",
     "url": "/i-website/dist/css/home.6efa1857.css"
   },
   {
-    "revision": "e8cbfb7100a943208a08",
+    "revision": "135362fb5c6396fe6938",
     "url": "/i-website/dist/css/index.b031e221.css"
   },
   {
-    "revision": "270d96e64381720d63d2",
+    "revision": "073fde04efeed91774e0",
     "url": "/i-website/dist/css/playground.33084e77.css"
   },
   {
@@ -72,7 +72,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/i-website/dist/fonts/codicon.805fb6ad.ttf"
   },
   {
-    "revision": "e7cf73657466bdb51c44fd4c868467c3",
+    "revision": "a825ea4bf8009e8d0c6c462ba6785e6c",
     "url": "/i-website/dist/home.html"
   },
   {
@@ -88,27 +88,27 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/i-website/dist/img/not-found.06512621.jpeg"
   },
   {
-    "revision": "fb202c79f306a4fb0336966b3d8908e6",
+    "revision": "d69cc0758c9dc512c10ac4d22b806dfb",
     "url": "/i-website/dist/index.html"
   },
   {
-    "revision": "658b69573aa84ab1932c",
-    "url": "/i-website/dist/js/home.d28f03.js"
+    "revision": "46c347e31279929209db",
+    "url": "/i-website/dist/js/home.080f23.js"
   },
   {
-    "revision": "e8cbfb7100a943208a08",
-    "url": "/i-website/dist/js/index.552db5.js"
+    "revision": "135362fb5c6396fe6938",
+    "url": "/i-website/dist/js/index.3b43fe.js"
   },
   {
-    "revision": "270d96e64381720d63d2",
-    "url": "/i-website/dist/js/playground.934666.js"
+    "revision": "073fde04efeed91774e0",
+    "url": "/i-website/dist/js/playground.57dcdd.js"
   },
   {
     "revision": "3866f3ee76597fcdadc88a83a339fdae",
     "url": "/i-website/dist/manifest.json"
   },
   {
-    "revision": "47d2a96727f4c14b63e9d58de1534c0c",
+    "revision": "217ff788aedc1fa543e2866cb2965cfd",
     "url": "/i-website/dist/playground.html"
   },
   {
