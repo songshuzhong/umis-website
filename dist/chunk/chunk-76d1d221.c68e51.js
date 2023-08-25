04f9eca2d5749856348738289e6cc519ce02431a
songshuzhong <sshuzhong@outlook,com>
2023-08-25 18:40:24 +0800

    update

---
+++ dist/chunk/chunk-76d1d221.c68e51.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-76d1d221"],{"042f":function(n,w,o){}}]);
---
+++ dist/precache-manifest.ff190d814a9dfc7993c163032633be73.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2398567077f330955f72",
+    "url": "/i-website/dist/chunk/chunk-2d0b8b3e.85d3ce.js"
+  },
+  {
+    "revision": "1d2ea4acf59a64025948",
+    "url": "/i-website/dist/chunk/chunk-2d0ccba9.9ea3ea.js"
+  },
+  {
+    "revision": "9e7f075bf5edf4d1368b",
+    "url": "/i-website/dist/chunk/chunk-2d0db7d7.1ade0d.js"
+  },
+  {
+    "revision": "56092de30c9ea44709ef",
+    "url": "/i-website/dist/chunk/chunk-2d208dfb.e2897a.js"
+  },
+  {
+    "revision": "fa5dfea3f2af8ea19f4a",
+    "url": "/i-website/dist/chunk/chunk-2d225f49.1a8097.js"
+  },
+  {
+    "revision": "17a5b476d83d80ac3b0b",
+    "url": "/i-website/dist/chunk/chunk-476d9766.fb89ca.js"
+  },
+  {
+    "revision": "bafe63f5cd9f4edb4976",
+    "url": "/i-website/dist/chunk/chunk-63ab760a.9427da.js"
+  },
+  {
+    "revision": "4b1a937db73814c92f84",
+    "url": "/i-website/dist/chunk/chunk-6ce51b6e.a03f47.js"
+  },
+  {
+    "revision": "27c6b7029ecd973027b3",
+    "url": "/i-website/dist/chunk/chunk-76d1d221.c68e51.js"
+  },
+  {
+    "revision": "71a0be0b851a93081022",
+    "url": "/i-website/dist/chunk/chunk-b2126b70.6a540e.js"
+  },
+  {
+    "revision": "84bee6da83c754cf914f",
+    "url": "/i-website/dist/chunk/chunk-vendors.01eb6c.js"
+  },
+  {
+    "revision": "ed6a3e46a8556d7446a6",
+    "url": "/i-website/dist/chunk/editor.08ddb6.js"
+  },
+  {
+    "revision": "ae48e2078551e8a64156f767f51e5254",
+    "url": "/i-website/dist/circle.svg"
+  },
+  {
+    "revision": "17a5b476d83d80ac3b0b",
+    "url": "/i-website/dist/css/chunk-476d9766.ba4ddaba.css"
+  },
+  {
+    "revision": "4b1a937db73814c92f84",
+    "url": "/i-website/dist/css/chunk-6ce51b6e.90717131.css"
+  },
+  {
+    "revision": "27c6b7029ecd973027b3",
+    "url": "/i-website/dist/css/chunk-76d1d221.c6997ae2.css"
+  },
+  {
+    "revision": "84bee6da83c754cf914f",
+    "url": "/i-website/dist/css/chunk-vendors.ccecdfeb.css"
+  },
+  {
+    "revision": "8b88591a09952c54adc1",
+    "url": "/i-website/dist/css/home.9d267e69.css"
+  },
+  {
+    "revision": "794f571e2d283513a249",
+    "url": "/i-website/dist/css/index.a45c11e5.css"
+  },
+  {
+    "revision": "27027df8237a1e0f6149",
+    "url": "/i-website/dist/css/playground.33084e77.css"
+  },
+  {
+    "revision": "805fb6ad1751ed8b849b5bf9be742ab5",
+    "url": "/i-website/dist/fonts/codicon.805fb6ad.ttf"
+  },
+  {
+    "revision": "833845d9f9e478e99b94afb15f4b4c8a",
+    "url": "/i-website/dist/home.html"
+  },
+  {
+    "revision": "ae48e2078551e8a64156f767f51e5254",
+    "url": "/i-website/dist/img/circle.ae48e207.svg"
+  },
+  {
+    "revision": "25b99dcac91b4c512353eccd69f391e4",
+    "url": "/i-website/dist/img/forbidden.25b99dca.jpeg"
+  },
+  {
+    "revision": "06512621b564df8be841598d77e6e195",
+    "url": "/i-website/dist/img/not-found.06512621.jpeg"
+  },
+  {
+    "revision": "13631926781b067cf95dcc29ec67b61a",
+    "url": "/i-website/dist/index.html"
+  },
+  {
+    "revision": "8b88591a09952c54adc1",
+    "url": "/i-website/dist/js/home.151cc0.js"
+  },
+  {
+    "revision": "794f571e2d283513a249",
+    "url": "/i-website/dist/js/index.b4d91e.js"
+  },
+  {
+    "revision": "27027df8237a1e0f6149",
+    "url": "/i-website/dist/js/playground.7cce60.js"
+  },
+  {
+    "revision": "3866f3ee76597fcdadc88a83a339fdae",
+    "url": "/i-website/dist/manifest.json"
+  },
+  {
+    "revision": "29553a16cc63574e85b448fdd985049c",
+    "url": "/i-website/dist/playground.html"
+  },
+  {
+    "revision": "9df39e09fcb3cb9b413a46f9ab221d71",
+    "url": "/i-website/dist/shuttle.png"
+  },
+  {
+    "revision": "96ae88e617004c7b4b4b8f42ac475a74",
+    "url": "/i-website/dist/worker/editor.worker.js"
+  },
+  {
+    "revision": "55a39908513ae7c0fc39a5f1ca0a8088",
+    "url": "/i-website/dist/worker/json.worker.js"
+  }
+]);
