4cde497332a2a442f0055f31d9fd04df9ba2fc15
songshuzhong <sshuzhong@outlook,com>
2023-10-19 11:24:00 +0800

    update

--- src/component/ToMobile.vue
+++ src/component/ToMobile.vue
@@ -9,20 +9,22 @@ export default defineComponent({
   setup() {
     const {proxy} = getCurrentInstance();
     const onClick = () => {
+      const isPro = process.env.NODE_ENV === 'production';
+      let pageId = '8f287653-1059-414b-9485-dffbfe91d2b0';
       if (window.IRenderer.pageInfo && window.IRenderer.pageInfo.pageId) {
-        const isPro = process.env.NODE_ENV === 'production';
-        proxy.$dispatchAction(
-          proxy,
-          {},
-          {
-            renderer: 'action',
-            actionType: 'url',
-            url: isPro? 'https://songshuzhong.github.io/i-website/dist/mobille.html?isFrame=true&pageId=' + window.IRenderer.pageInfo.pageId: `${window.location.protocol}//${window.location.host}/mobile.html?isFrame=true&pageId=${window.IRenderer.pageInfo.pageId}`
-          },
-          {},
-          () => {},
-        );
+        pageId = window.IRenderer.pageInfo.pageId;
       }
+      proxy.$dispatchAction(
+        proxy,
+        {},
+        {
+          renderer: 'action',
+          actionType: 'url',
+          url: isPro? 'https://songshuzhong.github.io/i-website/dist/mobille.html?isFrame=true&pageId=' + pageId: `${window.location.protocol}//${window.location.host}/mobile.html?isFrame=true&pageId=${pageId}`
+        },
+        {},
+        () => {},
+      );
     };
     return {
       onClick
