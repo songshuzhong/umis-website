76776d58528ae2fe853117137118dafc1bd0d61c
songshuzhong <sshuzhong@outlook,com>
2023-12-18 17:04:32 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -73,7 +73,7 @@ module.exports = {
     disableHostCheck: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:1026',
         changeOrigin: true
       }
     }
