cc5bd00ee67ff014fc34ea8ce0d73bba8639e4a3
songshuzhong <sshuzhong@outlook,com>
2024-01-03 07:32:36 +0800

    update

--- src/pages/mobile.js
+++ src/pages/mobile.js
@@ -9,10 +9,20 @@ import 'i-renderer/dist/css/index.css';
 import '../style/mobile.scss';
 
 const app = createApp(Application);
-
+const config = {
+  request: function(req) {
+    if (localStorage.getItem('token')) {
+      if (!req.headers) {
+        req.headers = {};
+      }
+      req.headers['Authorization'] = localStorage.getItem('token');
+    }
+    return req;
+  }
+};
 registrySw(process.env.VUE_APP_SERVICE_WORKER);
 
 app
   .use(ElementPlus)
-  .use(IRenderer)
+  .use(IRenderer, config)
   .mount('.i-website-app__container');
