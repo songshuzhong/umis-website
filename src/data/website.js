46b762f755d5c55f886982dbca6cb2c5e251266e
songshuzhong <sshuzhong@outlook,com>
2024-01-03 10:59:45 +0800

    update

--- src/data/website.js
+++ src/data/website.js
@@ -53,12 +53,12 @@ export default {
   },
   {
     "renderer": "html",
-    "classname": "margin-bottom-10 padding-right-20",
+    "classname": "margin-bottom-10 padding-left-20",
     "html": "- 🤔 When you arise in the morning,think of what a precious privilege it is to be alive - to breathe,to think,to enjoy,to love. ~Marcus Aurelius."
   },
   {
     "renderer": "html",
-    "classname": "margin-bottom-20 padding-right-20",
+    "classname": "margin-bottom-20 padding-left-20",
     "html": "- 😄 当你早晨起来的时候，想想活着是多么宝贵的特权——还能呼吸，还能思考，还能享受，还能去爱。~马尔克·奥列里乌斯。"
   },
   {
