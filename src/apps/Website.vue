cd4a423e29f6cf29fc3761b90a918e6f9de56ae0
songshuzhong <sshuzhong@outlook,com>
2024-01-11 14:32:28 +0800

    KABP-3667

--- src/apps/Website.vue
+++ src/apps/Website.vue
@@ -57,7 +57,7 @@ export default defineComponent({
     });
     onMounted(() => {
       const timer = setTimeout(() => {
-        /*import('../../../i-renderer/packages/canvas/index')
+        import('../../../i-renderer/packages/canvas/index')
           .then(res => {
             const {Editor} = res;
             proxy.$.appContext.components[Editor.name] = Editor;
@@ -65,7 +65,7 @@ export default defineComponent({
             console.error(e);
           }).finally(() => {
             clearTimeout(timer);
-          });*/
+          });
         appendAssets();
         clearTimeout(timer);
       }, 5000);
