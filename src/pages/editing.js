3ae023e5d9d3d33499073099b2c0d8bff674343a
songshuzhong <sshuzhong@outlook,com>
2024-01-11 11:22:19 +0800

    feature:editing

---
+++ src/pages/editing.js
@@ -0,0 +1,18 @@
+import {createApp} from 'vue';
+import ElementPlus from 'element-plus';
+import IRenderer from 'i-renderer/dist/js/renderer';
+import Application from '../apps/Editing.vue';
+import registrySw from '../registerServiceWorker';
+
+import 'element-plus/dist/index.css';
+import 'element-plus/theme-chalk/dark/css-vars.css';
+import 'i-renderer/dist/css/index.css';
+
+const app = createApp(Application);
+
+registrySw(process.env.VUE_APP_SERVICE_WORKER);
+
+app
+  .use(ElementPlus)
+  .use(IRenderer)
+  .mount('.i-website-app__container');
