bc08310ea3323d853be3982f69c9eb8bfda47a19
songshuzhong <18346667355@163.com>
2024-05-18 06:41:35 +0800

    m

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
 
 npm run build
-sudo /etc/nginx -s stop
-echo 'nginx stop success'
 sudo rm -rf /usr/share/nginx/html/i-website/
 sudo mkdir /usr/share/nginx/html/i-website
 sudo mv /home/lighthouse/github/i-website/dist/* /usr/share/nginx/html/i-website/
-sudo /etc/nginx
+sudo nginx -s reload
 echo 'nginx start success'
