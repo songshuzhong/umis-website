c3d7eaecdcb2f57d2426ab5238cd8396792c3aae
songshuzhong <18346667355@163.com>
2024-05-17 20:38:00 +0800

    a

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 npm run build
-sudo /usr/local/nginx/sbin/nginx -s stop
+sudo /etc/nginx -s stop
 echo 'nginx stop success'
-sudo rm -rf /usr/local/nginx/html/i-website/
-sudo mkdir /usr/local/nginx/html/i-website
-sudo mv /home/lighthouse/github/i-website/dist/* /usr/local/nginx/html/i-website/
-sudo /usr/local/nginx/sbin/nginx
+sudo rm -rf /usr/share/nginx/html/i-website/
+sudo mkdir /usr/share/nginx/html/i-website
+sudo mv /home/lighthouse/github/i-website/dist/* /usr/share/nginx/html/i-website/
+sudo /etc/nginx
 echo 'nginx start success'
