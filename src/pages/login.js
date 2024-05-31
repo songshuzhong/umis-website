0829b1d7eb24ec8307efd905a587313a893c1855
songshuzhong <18346667355@163.com>
2024-05-31 21:13:08 +0800

    update

--- src/pages/login.js
+++ src/pages/login.js
@@ -14,34 +14,52 @@ import '../style/login.scss';
 
 const app = createApp(Application);
 
+const onLogin = (proxy, config, context, onActionFeedback, formdata, form) => {
+  api().staticApi()
+    .post(`${process.env.VUE_APP_API_BASE}/api/auth/login`, formdata)
+    .then((res) => {
+      localStorage.setItem('token', `Bearer ${res.data}`);
+      const url = process.env.NODE_ENV === 'dev'? 'localhost/website.html': 'localhost/website';
+      proxy.$dispatchAction(proxy, {url, actionType: 'url'}, {}, () => {});
+    })
+    .catch((e) => {
+      form && form.$parent.$refs.field[4].$refs.component.handleDraw();
+      ElNotification({
+        title: `错误${e?.data?.code || e?.response?.data?.code || e.code}`,
+        message: e?.data?.message || e?.response?.data?.message || e.message,
+        type: 'error',
+        duration: 10000,
+        offset: 50
+      });
+    })
+    .finally(() => {
+      onActionFeedback&&onActionFeedback('CANCEL_LOADING');
+    });
+};
 const options = {
   domains: [process.env.VUE_APP_API_BASE],
   renderers: [Sendemail, Verify],
   actions: {
-    login: function (proxy, props, context, onActionFeedback) {
+    visitor: function(proxy, config, context, onActionFeedback) {
+      onLogin(proxy, config, context, onActionFeedback, process.env.NODE_ENV === 'dev'? {
+        namespace: '联通',
+        username: 'admin',
+        password: 'admin',
+        code: '1234',
+        verify: '1234'
+      }: {
+        namespace: 'guest',
+        username: 'guest',
+        password: '_123456qwerty',
+        code: '1234',
+        verify: '1234'
+      });
+    },
+    login: function (proxy, config, context, onActionFeedback) {
       const form = proxy.$parent.$parent.$parent;
       form.validate((valid) => {
         if (valid) {
-          api()
-            .staticApi()
-            .post(`${process.env.VUE_APP_API_BASE}/api/auth/login`, form.$parent.state.data)
-            .then((res) => {
-              localStorage.setItem('token', `Bearer ${res.data}`);
-              proxy.$dispatchAction(proxy, {url: 'localhost/website', actionType: 'url'}, {}, () => {});
-            })
-            .catch((e) => {
-              form.$parent.$refs.field[4].$refs.component.handleDraw();
-              ElNotification({
-                title: `错误${e?.data?.code || e?.response?.data?.code || e.code}`,
-                message: e?.data?.message || e?.response?.data?.message || e.message,
-                type: 'error',
-                duration: 10000,
-                offset: 50
-              });
-            })
-            .finally(() => {
-              onActionFeedback&&onActionFeedback('CANCEL_LOADING');
-            });
+          onLogin(proxy, config, context, onActionFeedback, form.$parent.state.data, form);
         } else {
           onActionFeedback&&onActionFeedback('CANCEL_LOADING');
         }
