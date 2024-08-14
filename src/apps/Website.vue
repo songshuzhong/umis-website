a6b9a5af291f3d942bdb96d70a87cde7685f8089
songshuzhong <18346667355@163.com>
2024-08-14 20:26:14 +0800

    feature: admin

--- src/apps/Website.vue
+++ src/apps/Website.vue
@@ -9,7 +9,7 @@
 <script>
 import {defineComponent, onBeforeMount, onMounted, getCurrentInstance} from 'vue';
 import {Schema} from '../../../i-renderer/packages/index';
-import frameSchema from '../data/websiteFrame1.json';
+import frameSchema from '../data/websiteFrame.json';
 
 export default defineComponent({
   name: 'Application',
